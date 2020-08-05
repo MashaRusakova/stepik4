742bb503d34308124826c43a8826fd797fb56087
MashaRusakova <masharusakova87@mail.ru>
2020-08-05 20:12:26 +0300

    Commit2

--- beautyboxes/beautyboxes/urls.py
+++ beautyboxes/beautyboxes/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('api/beautyboxes/<int:pk>/', BeautyboxDetail.as_view(), name='beautybox-detail'),
     path('api/recipients/', RecipientList.as_view(), name='recipient-List'),
     path('api/recipients/<int:pk>/', RecipientDetail.as_view(), name='recipient-detail'),
-    path('api/recipients/<int:pk>/change_snp', RecipientChangeSNP.as_view(), name='recipientsnd-detail'),
-    path('api/orders', OrderList.as_view(), name='order-List'),
+    path('api/recipients/<int:pk>/change_snp/', RecipientChangeSNP.as_view(), name='recipientsnd-detail'),
+    path('api/orders/', OrderList.as_view(), name='order-List'),
     path('api/orders/<int:pk>/', OrderDetail.as_view(), name='order-detail'),
-    path('api/orders/<int:pk>/change_address', OrderChangeAddress.as_view(), name='change-address'),
+    path('api/orders/<int:pk>/change_address/', OrderChangeAddress.as_view(), name='change-address'),
 ]
 
